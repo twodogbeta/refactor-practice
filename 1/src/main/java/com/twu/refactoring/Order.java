bbb417fd581cf3d110220f91593b75648947b208
twodogbeta <qijinhaoup@163.com>
2020-08-03 22:53:53 +0800

    finish exercise1 name

--- 1/src/main/java/com/twu/refactoring/Order.java
+++ 1/src/main/java/com/twu/refactoring/Order.java
@@ -3,25 +3,25 @@ package com.twu.refactoring;
 import java.util.List;
 
 public class Order {
-    String nm;
-    String addr;
-    List<LineItem> li;
+    String name;
+    String address;
+    List<LineItem> list;
 
-    public Order(String nm, String addr, List<LineItem> li) {
-        this.nm = nm;
-        this.addr = addr;
-        this.li = li;
+    public Order(String name, String address, List<LineItem> list) {
+        this.name = name;
+        this.address = address;
+        this.list = list;
     }
 
     public String getCustomerName() {
-        return nm;
+        return name;
     }
 
     public String getCustomerAddress() {
-        return addr;
+        return address;
     }
 
     public List<LineItem> getLineItems() {
-        return li;
+        return list;
     }
 }
